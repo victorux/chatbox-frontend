7333698e54117ddadbca2e3802098f3b2d0fb307
victorb <hi.victorb@gmail.com>
2022-11-24 15:24:25 +0000

    removing boilerplate code

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,13 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>This is Home Page
-      <h1>Heading 1</h1>
-      <h2>Heading 1</h2>
-      <h3>Heading 1</h3>
-      <h4>Heading 1</h4>
-      <h5>Heading 1</h5>
-    </div>
+    <div>This is Home Page</div>
   )
 }
 
