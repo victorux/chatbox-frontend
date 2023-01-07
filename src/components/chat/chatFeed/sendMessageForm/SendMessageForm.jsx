781168f1ddf931cd384ceac4f00cc24c7f714310
victorb <hi.victorb@gmail.com>
2023-01-07 22:52:22 +0000

    Send message form

--- src/components/chat/chatFeed/sendMessageForm/SendMessageForm.jsx
+++ src/components/chat/chatFeed/sendMessageForm/SendMessageForm.jsx
@@ -1,8 +1,42 @@
-import { Container } from "./sendMessageForm.styled"
+import { useFormik } from "formik"
+// import * as Yup from 'yup';
+
+import { 
+  Button,
+  ComposeMessageContainer,
+  Container,
+  CustomFrom,
+  TextArea 
+} from "./sendMessageForm.styled"
 
 function SendMessageForm() {
+  const { values, handleChange } = useFormik({
+    initialValues: {
+      message: ''
+    }
+  })
+  console.log(values.message);
   return (
-    <Container>SendMessageForm</Container>
+    <Container>
+      <CustomFrom autoComplete="off">
+        <ComposeMessageContainer>
+          <TextArea 
+            value={values.message}
+            onChange={handleChange}
+            id="message"
+            name="message"
+            placeholder="Type something..."
+            rows={5}
+          />
+        </ComposeMessageContainer>
+        <div>
+          <Button
+            type="submit" 
+            disabled={values.message.length > 0 ? false : true}
+          >Send</Button>
+        </div>
+      </CustomFrom>
+    </Container>
   )
 }
 
