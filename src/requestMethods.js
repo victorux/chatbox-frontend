f4db2ba124665ad281a7fafe38fe35fd626e1e3d
victorb <hi.victorb@gmail.com>
2023-02-09 20:06:02 +0000

    update request methods

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://chabox-server.onrender.com/api"
 
-const user = JSON.parse(localStorage.getItem("persist:root"))?.currentUser;
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 
